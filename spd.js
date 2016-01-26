8633068443d48e7975048d5de71b04174d6d1b14
Al7otweb <mdwnelgohary@gmail.com>
2016-01-26 13:08:47 +0200

    Update spd.js

--- spd.js
+++ spd.js
@@ -1,1 +1,9 @@
-<script src='https://googledrive.com/host/0B5Z7dU1X-KejdlJObkFvMV9SQTA' type='text/javascript'></script><canvas id='scratch' style='width: 500px; height: 350px; margin-left: -100px; margin-top: -30px;'></canvas><script src='https://googledrive.com/host/0B5Z7dU1X-KejVEN3T3d1REVfajg' type='text/javascript'></script><style>#scratch {position: ;top: 0px;left: 0px;}</style>
+<cenves>
+<script src='https://googledrive.com/host/0B5Z7dU1X-KejdlJObkFvMV9SQTA' type='text/javascript'>
+</script>
+<canvas id='scratch' style='height: 350px; margin-left: -100px; margin-top: -30px;'>
+</canvas>
+<script src='https://googledrive.com/host/0B5Z7dU1X-KejVEN3T3d1REVfajg' type='text/javascript'>
+</script>
+<style>#scratch {position: ;top: 0px;left: 0px;}</style>
+</cenves>
