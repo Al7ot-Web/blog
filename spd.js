b222313d4509b18540b6417cb073a17174a0b364
Al7otweb <mdwnelgohary@gmail.com>
2016-05-03 13:21:32 +0200

    Delete spd.js

--- spd.js
+++
@@ -1,9 +0,0 @@
-<cenves>
-<script src='https://googledrive.com/host/0B5Z7dU1X-KejdlJObkFvMV9SQTA' type='text/javascript'>
-</script>
-<canvas id='scratch' style='height: 350px; margin-left: -100px; margin-top: -30px;'>
-</canvas>
-<script src='https://googledrive.com/host/0B5Z7dU1X-KejVEN3T3d1REVfajg' type='text/javascript'>
-</script>
-<style>#scratch {position: ;top: 0px;left: 0px;}</style>
-</cenves>
